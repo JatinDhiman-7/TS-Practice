cfb85b25acf56fb9c5e691ae8f0491fbb0c7e743
Jatin <jatin.careeer@gmail.com>
2026-05-25 21:36:08 +0530

    practice

---
+++ Practice.js
@@ -0,0 +1,23 @@
+"use strict";
+let Name = "Jatin Dhiman";
+let age = 22;
+console.log(Name);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(28, 40));
+let data = ["Jatin", "Dhiman", "Rohit", "Satyarth"];
+console.log(data);
+let id = 1234;
+console.log(id);
+let value = 10;
+function multiply(num) {
+    return num * 2;
+}
+multiply(value);
+// interface Person{
+//     name:string,
+//     Age:number,
+//     Qulification:number,
+//     isPG:boolean,
+// }
--- Practice.ts
+++ Practice.ts
@@ -1,10 +1,31 @@
+// let Name:string="Jatin Dhiman";
+// let age:number=22;
+// console.log(Name);
 
 
 
+// function add(a:number,b:number):number{
+//     return a+b
+// }
 
-interface Person{
-    name:string,
-    Age:number,
-    Qulification:number,
-    isPG:boolean,
-}
+// console.log(add(28,40));
+
+// let data:string[]=["Jatin","Dhiman","Rohit","Satyarth"];
+// console.log(data);
+
+// let id:string|number=1234;
+// console.log(id);
+
+// let value:number=10;
+// function multiply(num:number):number{
+//     return num*2;
+// }
+// multiply(value);
+
+
+// interface Person{
+//     name:string,
+//     Age:number,
+//     Qulification:number,
+//     isPG:boolean,
+// }
